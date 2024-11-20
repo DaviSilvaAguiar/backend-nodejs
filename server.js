bd19667b654a091c4f602af7fad394df73da417f
DaviSilvaAguiar <daviagui13@gmail.com>
2024-11-20 16:33:33 -0300

    Conectando API ao MongoDB

--- server.js
+++ server.js
@@ -1,39 +1,11 @@
 import express from "express";
+import routes from './src/routes/postsRoutes.js';
 
-const posts = [
-    {
-      id: 1,
-      descricao: "Uma foto teste",
-      imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-      id: 2,
-      descricao: "Gato fazendo yoga",
-      imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-      id: 3,
-      descricao: "Paisagem incrível!",
-      imagem: "https://placecats.com/millie/300/150"
-    },
-  ];
-
+// Criação da aplicação Express
 const app = express();
+routes(app);
+
+// Inicia o servidor na porta 3000
 app.listen(3000, () => {
-    console.log("Servidor escutando...");
-});
-
-function buscarPostPorID(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    })
-}
-
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorID(req.params.id)
-    res.status(200).json(posts[index]);
+    console.log("servidor escutando...");
 });
