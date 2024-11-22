f1f74229be74da819831b5e09a90862e02ebec56
DaviSilvaAguiar <daviagui13@gmail.com>
2024-11-22 19:53:22 -0300

    Implementando armazenamento e upload de imagens

--- src/config/routes/postRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { listarPosts } from "../controllers/postController.js";
- const routes = (app) => {
-  app.use(express.json()); // Permite o uso de JSON nas requisições
-  // Rota para obter todos os posts
-  app.get("/posts",listarPosts);
-};
-export default routes;
